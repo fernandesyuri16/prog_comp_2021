7253fe61e3c4ff5df49512b0d9112c2876a5d78f
Yuri Fernandes <89111833+fernandesyuri16@users.noreply.github.com>
2021-10-05 22:16:58 -0300

    05-10

---
+++ 05-10.js
@@ -0,0 +1,27 @@
+//cria a função
+let sistemaVendas = () => {
+    //vetor de meses
+    let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto",
+    "Setembro","Outubro","Novembro","Dezembro"]
+    
+    //declara a matriz
+    let matriz = []
+
+    for(let i = 0; i < 12; i++){ // para cada mês
+        matriz[i] = []
+        alert(`Informe as vendas do mês ${meses[i]}`)
+        for(let j = 0; j < 4; j++){
+            matriz[i][j] = Number(prompt(`Informe o valor vendido na ${j + 1}ª semana `))
+        }
+    }
+    
+    let soma
+    for(let i = 0; i < 12; i++){ // para cada mês
+        soma = 0
+        for(let j = 0; j < 4; j++){
+            soma = soma + matriz[i][j]
+        }
+        // acabou de somar o mês
+        alert(`Total vendido no Mês ${meses[i]}: \nR$ ${soma}`)
+    }
+}
